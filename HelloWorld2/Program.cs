ec341da82777df6a05c522ce6cf63aabeea85309
Machina <machina123@boun.cr>
2014-09-30 16:28:43 +0200

    Code rework

--- HelloWorld2/Program.cs
+++ HelloWorld2/Program.cs
@@ -9,91 +9,7 @@ namespace HelloWorld2
 {
     class Program
     {
-        public const int MAX_SIZE = 5;
-
-        public static double[,] AddMatrix(double[,] A, double[,] B)
-        {
-            double[,] C = new double[MAX_SIZE, MAX_SIZE];
-
-            for (int i = 0; i < MAX_SIZE; i++)
-            {
-                for (int j = 0; j < MAX_SIZE; j++)
-                {
-                    C[i, j] = A[i, j] + B[i, j];
-                }
-            }
-
-            return C;
-        }
-
-        /* SubtractMatrix
-        public static double[,] SubtractMatrix(double[,] A, double[,] B)
-        {
-            double[,] C = new double[MAX_SIZE, MAX_SIZE];
-
-            for (int i = 0; i < MAX_SIZE; i++)
-            {
-                for (int j = 0; j < MAX_SIZE; j++)
-                {
-                    C[i, j] = A[i, j] - B[i, j];
-                }
-            }
-
-            return C;
-        }
-        */
-
-        public static double[,] MultipyMatrixByNumber(double[,] A, double k)
-        {
-            double[,] C = new double[MAX_SIZE, MAX_SIZE];
-
-            for (int i = 0; i < MAX_SIZE; i++)
-            {
-                for (int j = 0; j < MAX_SIZE; j++)
-                {
-                    C[i, j] = A[i, j] * k;
-                }
-            }
-
-            return C;
-        }
-       
-        /* DivideMatrixByNumber
-        public static double[,] DivideMatrixByNumber(double[,] A, double k)
-        {
-            double[,] C = new double[MAX_SIZE, MAX_SIZE];
-
-            for (int i = 0; i < MAX_SIZE; i++)
-            {
-                for (int j = 0; j < MAX_SIZE; j++)
-                {
-                    C[i, j] = A[i, j] / k;
-                }
-            }
-
-            return C;
-        }
-        */
-
-        public static void PrintMatrix(double[,] M)
-        {
-            for (int i = 0; i < MAX_SIZE; i++)
-            {
-                for (int j = 0; j < MAX_SIZE; j++)
-                {
-                    Console.Write(M[i, j] + " ");
-                }
-                Console.Write(Environment.NewLine);
-            }
-        }
-
-        public static double[,] MultiplyMatrix(double[,] A, double[,] B, int m, int n, int k)
-        {
-            double[,] C = new double[m,k];
-            return C;
-        }
-
-
+        
         static void Main(string[] args)
         {
             int m, n1, n2, k;
@@ -115,52 +31,110 @@ namespace HelloWorld2
                 Console.WriteLine("ErRoR! Macierzy nie można pomnożyć!");
             }
         }
+    }
 
-        static void Psychodelia()
+    class Matrix
+    {
+        private const int MAX_SIZE = 5;
+
+        public static double[,] CreateMatrix(int m, int n)
         {
-            const int size = 79;
-            char[,] tab = new char[size, size];
+            double[,] C = new double[m, n];
 
-
-            for (int i = 0; i < size; i++)
+            for (int i = 0; i < m; i++)
             {
-                for (int j = 0; j < size; j++)
+                for (int j = 0; j < n; j++)
                 {
-                    if (i == j || i == size - j - 1)
-                    {
-                        tab[i, j] = '#';
-                    }
-                    else
-                    {
-                        tab[i, j] = ' ';
-                    }
+                    Console.Write("Wprowadź element [{0},{1}]: ", i, j);
+                    C[i, j] = int.Parse(Console.ReadLine());
                 }
             }
 
-            Random r = new Random();
-            //while (true)
-            //{
-            for (int i = 0; i < size; i++)
+                return C;
+        }
+
+        public static double[,] AddMatrix(double[,] A, double[,] B, int m, int n)
+        {
+            double[,] C = new double[m, n];
+
+            for (int i = 0; i < m; i++)
             {
-                for (int j = 0; j < size; j++)
+                for (int j = 0; j < n; j++)
                 {
-                    if (tab[i, j] == '#')
-                    {
-                        Console.ForegroundColor = (ConsoleColor)r.Next(0, 15);
-                        Console.BackgroundColor = (ConsoleColor)r.Next(0, 15);
-                    }
-                    else
-                    {
-                        Console.ForegroundColor = ConsoleColor.Gray;
-                        Console.BackgroundColor = ConsoleColor.Black;
-                    }
-                    Console.Write(tab[i, j]);
+                    C[i, j] = A[i, j] + B[i, j];
+                }
+            }
+
+            return C;
+        }
+
+        public static void PrintMatrix(double[,] M)
+        {
+            for (int i = 0; i < MAX_SIZE; i++)
+            {
+                for (int j = 0; j < MAX_SIZE; j++)
+                {
+                    Console.Write(M[i, j] + " ");
                 }
                 Console.Write(Environment.NewLine);
             }
-            //}
-            Console.ReadKey();
-        }       
+        }
+
+        public static double[,] MultipyMatrixByNumber(double[,] A, double k, int m, int n)
+        {
+            double[,] C = new double[m, n];
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    C[i, j] = A[i, j] * k;
+                }
+            }
+
+            return C;
+        }
+
+        public static double[,] MultiplyMatrix(double[,] A, double[,] B, int m, int n, int k)
+        {
+            double[,] C = new double[m, k];
+            return C;
+        }
+
+        /* SubtractMatrix
+        public static double[,] SubtractMatrix(double[,] A, double[,] B)
+        {
+            double[,] C = new double[MAX_SIZE, MAX_SIZE];
+
+            for (int i = 0; i < MAX_SIZE; i++)
+            {
+                for (int j = 0; j < MAX_SIZE; j++)
+                {
+                    C[i, j] = A[i, j] - B[i, j];
+                }
+            }
+
+            return C;
+        }
+        */
+
+        /* DivideMatrixByNumber
+        public static double[,] DivideMatrixByNumber(double[,] A, double k)
+        {
+            double[,] C = new double[MAX_SIZE, MAX_SIZE];
+
+            for (int i = 0; i < MAX_SIZE; i++)
+            {
+                for (int j = 0; j < MAX_SIZE; j++)
+                {
+                    C[i, j] = A[i, j] / k;
+                }
+            }
+
+            return C;
+        }
+        */
+
     }
 }
 #endregion Program
